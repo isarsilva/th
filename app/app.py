acbb48d082dd7c893393956b4cde9f0b7401821e
isarsilva <94862299+isarsilva@users.noreply.github.com>
2025-07-02 10:01:36 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -17,5 +17,5 @@ async def app_init():
     cliente_db = AsyncIOMotorClient(settings.MONGO_CONNECTION_STRING).todoapp # Criando uma instância do cliente MongoDB usando a string de conexão definida nas configurações
 
     await init_beanie(database=cliente_db, document_models=[ # Aqui você deve definir os modelos de documento que serão usados com o Beanie
-      ])  # Inicializando o Beanie com o banco de dados e os modelos de documento    
+      ])  # Inicializando o Beanie com o banco de dados e os modelos de documento...
      
